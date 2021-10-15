83fe5bd90fc9380f83abe484b92e8d3fd410ccfb
MidasXIV <amanshaikh.arc@gmail.com>
2021-10-16 00:00:23 +0400

    implement logic to find new coins on binance

--- src/daemons/services/newCoinListingService.ts
+++ src/daemons/services/newCoinListingService.ts
@@ -1,10 +1,40 @@
-import Service, { TIME_INTERVAL } from "./serviceInterface";
+import Discord, { TextChannel } from "discord.js";
+import Service, { AbstractService, TIME_INTERVAL } from "./serviceInterface";
 import { Message } from "discord.js";
+import { allCoins } from "../../api/binance/allcoins";
+import loki from "lokijs";
 
-export class newCoinListingService implements Service {
+interface pairEntry {
+  pair: string,
+  price: number
+}
+
+interface coinListing {
+  [key: string]: number
+}
+export class newCoinListingService extends AbstractService implements Service {
 
   name = "new-coin-listing-service";
   executionInterval = TIME_INTERVAL.SECONDS_30;
+  coinListing: Collection<any>;
+  db: LokiConstructor;
+
+  constructor(client: Discord.Client) {
+
+    super(client);
+
+    this.db = new loki('anavrin.db', {
+      autoload: false,
+      autosave: true,
+      autosaveInterval: 4000 // save every four seconds for our example
+    });
+
+    this.coinListing = this.db.getCollection("coin-listing")
+    if (this.db.getCollection("coin-listing") === null) {
+      this.coinListing = this.db.addCollection("coin-listing")
+    }
+
+  }
 
   help(commandPrefix: string): string {
     return `Use ${commandPrefix}greet to get a greeting.`;
@@ -16,6 +46,33 @@ export class newCoinListingService implements Service {
 
   async register(): Promise<void> {
     console.log("Running new coin listing deamon");
+    let entryCount = this.coinListing.count();
+
+    const coins: coinListing = await JSON.parse(await allCoins());
+    const coinPairings = Object.keys(coins).reduce((acc: Array<pairEntry>, pair: string) => {
+      const coinPairing: pairEntry = {
+        pair,
+        price: coins[pair]
+      }
+      return [...acc, coinPairing];
+    }, []);
+
+    // store coins in database as [{pair: "ETHUSDT", price: 3000}, {}]
+
+    if (entryCount === 0) {
+      // database has no data
+      this.log("database has no data");
+      this.coinListing.insert(coinPairings);
+      entryCount = this.coinListing.count();
+    }
+
+    coinPairings.forEach(coinPairing => {
+      const pairingName = coinPairing.pair;
+      const isPairListed = Boolean(this.coinListing.findOne({ pair: pairingName }));
+      if (!isPairListed) {
+        this.log(`New pair listed on binance : ${pairingName}`);
+      }
+    });
     return;
   }
 
