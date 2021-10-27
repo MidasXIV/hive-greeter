6146809d17a5a5761820dd55c690f6ef9e33ff5b
Brian Gates <brian.g.gates@gmail.com>
2021-10-27 16:52:14 -0700

    added !invade

--- src/commandHandler.ts
+++ src/commandHandler.ts
@@ -1,22 +1,17 @@
 import { Message } from "discord.js";
-import { GreetCommand, TimeCommand } from "./commands";
+import { BotinvasionCommand, GreetCommand, TimeCommand } from "./commands";
 import Command from "./commands/commandInterface";
 import { CommandParser } from "./models/commandParser";
 
 export default class CommandHandler {
-
   private commands: Command[];
 
   private readonly prefix: string;
 
   constructor(prefix: string) {
+    const commandClasses = [GreetCommand, TimeCommand, BotinvasionCommand];
 
-    const commandClasses = [
-      GreetCommand,
-      TimeCommand
-    ];
-
-    this.commands = commandClasses.map(commandClass => new commandClass());
+    this.commands = commandClasses.map((commandClass) => new commandClass());
     this.prefix = prefix;
   }
 
@@ -26,17 +21,19 @@ export default class CommandHandler {
       return;
     }
 
-    message.reply(`Hive Greeter recieved '${this.echoMessage(message)}' from ${message.author.tag}`);
-
     const commandParser = new CommandParser(message, this.prefix);
 
-    const matchedCommand = this.commands.find(command => command.commandNames.includes(commandParser.parsedCommandName));
+    const matchedCommand = this.commands.find((command) =>
+      command.commandNames.includes(commandParser.parsedCommandName)
+    );
 
     if (!matchedCommand) {
       await message.reply(`I don't recognize that command. Try !help.`);
     } else {
-      await matchedCommand.run(message).catch(error => {
-        message.reply(`'${this.echoMessage(message)}' failed because of ${error}`);
+      await matchedCommand.run(message).catch((error) => {
+        message.reply(
+          `'${this.echoMessage(message)}' failed because of ${error}`
+        );
       });
     }
   }
---
+++ src/commands/botinvasionCommand.ts
@@ -0,0 +1,21 @@
+import Command from "./commandInterface";
+import { Message } from "discord.js";
+
+const gifUrl =
+  "https://media4.giphy.com/media/pIMlKqgdZgvo4/giphy.gif?cid=ecf05e47bjo8bxw0dpavli0ry7kj3uaorkik3m1cw0hmvc1z&rid=giphy.gif&ct=g";
+
+const gifFile = "D:\\Downloads\\botinvasion.gif";
+
+export class BotinvasionCommand implements Command {
+  commandNames = ["invade"];
+
+  help(commandPrefix: string): string {
+    return `Use ${commandPrefix}invade to summon a bot invasion.`;
+  }
+
+  async run(message: Message): Promise<void> {
+    await message.reply("BEEP BOOP THIS IS A TAKEOVER!", {
+      files: [gifFile],
+    });
+  }
+}
