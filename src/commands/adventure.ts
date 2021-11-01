5cd42f6908e6e22f96623e176dc0a2c112ad16cf
Brian Gates <brian.g.gates@gmail.com>
2021-11-01 12:27:14 -0700

    adventure embeds

--- src/commands/adventure.ts
+++ src/commands/adventure.ts
@@ -1,5 +1,5 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
-import { CommandInteraction } from "discord.js";
+import { CommandInteraction, Message, MessageEmbed } from "discord.js";
 import {
   adjustHP,
   getUserCharacter,
@@ -21,45 +21,88 @@ export const execute = async (
   const roll = Math.random();
   const player = getUserCharacter(initiator);
   if (player.hp === 0) {
-    await interaction.reply(`You're too weak to press on.`);
+    await interaction.reply({
+      embeds: [
+        new MessageEmbed().setDescription(`You're too weak to press on.`),
+      ],
+    });
     return;
   }
   if (isCharacterOnCooldown(player.id)) {
-    await interaction.reply(`You can't do that yet.`);
+    await interaction.reply({
+      embeds: [new MessageEmbed().setDescription(`You can't do that yet.`)],
+    });
   }
   setCharacterCooldown(initiator.id);
   if (roll >= 0.95) {
     levelup(initiator.id);
-    await interaction.reply(
-      `You encounter a divine entity that blesses you with +1 max hp!`
-    );
+    await interaction.reply({
+      embeds: [
+        new MessageEmbed()
+          .setDescription(
+            `You encounter a divine entity that blesses you with +1 max hp!`
+          )
+          .setImage("https://imgur.com/psnFPYG.png"),
+      ],
+    });
     return;
   }
   if (roll >= 0.8) {
     const healAmount = Math.floor(Math.random() * 6);
     adjustHP(initiator.id, healAmount);
-    await interaction.reply(
-      `You drink from a fairy's well, it heals you for ${healAmount}!`
-    );
+    await interaction.reply({
+      embeds: [
+        new MessageEmbed()
+          .setDescription(
+            `You drink from a fairy's well, it heals you for ${healAmount}!`
+          )
+          .setImage("https://imgur.com/bgq63v9.png"),
+      ],
+    });
     return;
   }
   if (roll >= 0.3) {
+    const message = await interaction.reply({
+      embeds: [
+        new MessageEmbed()
+          .setDescription(`It's a trap!`)
+          .setImage("https://imgur.com/TDMLxyE.png"),
+      ],
+      fetchReply: true,
+    });
+    if (!(message instanceof Message)) return;
     const result = trap(initiator.id);
-    await interaction.reply(`It's a trap! ...`);
+    // await interaction.reply(`It's a trap! ...`);
     await sleep(2000);
     switch (result.outcome) {
       case "hit":
-        await interaction.followUp(
-          `You're hit! You take ${result.damage} damage!`
-        );
+        await interaction.followUp({
+          embeds: [
+            new MessageEmbed()
+              .setDescription(`You're hit! You take ${result.damage} damage!`)
+              .setImage("https://imgur.com/28oehQm.png"),
+          ],
+        });
         break;
       case "miss":
-        await interaction.followUp(`You deftly evade!`);
+        await interaction.followUp({
+          embeds: [
+            new MessageEmbed()
+              .setDescription(`You deftly evade!`)
+              .setImage("https://imgur.com/gSgcrnN.png"),
+          ],
+        });
         break;
     }
     return;
   }
-  await interaction.reply(`You travel the lands.`);
+  await interaction.reply({
+    embeds: [
+      new MessageEmbed()
+        .setDescription(`You travel the lands.`)
+        .setImage("https://imgur.com/WCVVyh6.png"),
+    ],
+  });
 };
 
 export default { command, execute };
