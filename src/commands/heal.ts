512e342955c52b1515d85a83852d6c071582e644
Brian Gates <brian.g.gates@gmail.com>
2021-11-13 16:11:55 -0800

    🤍

--- src/commands/heal.ts
+++ src/commands/heal.ts
@@ -39,7 +39,7 @@ export const execute = async (
         embeds: [
           new MessageEmbed()
             .setTitle(`Heal`)
-            .setDescription(`Healed ${target} for ${result.amount}!`)
+            .setDescription(`Healed ${target} for 🤍${result.amount}!`)
             .setImage("https://i.imgur.com/S32LDbM.png")
             .addFields([hpBarField(getUserCharacter(target), result.amount)]),
         ],
