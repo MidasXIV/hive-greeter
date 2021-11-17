269edbd5d571cd21423ef7bacd30379af23fedc2
Brian Gates <brian.g.gates@gmail.com>
2021-11-16 16:28:20 -0800

    remove noisy log

--- src/heavyCrown/hasCrown.ts
+++ src/heavyCrown/hasCrown.ts
@@ -1,10 +1,4 @@
 import { Character } from "../character/Character";
 
-export const hasCrown = (character: Character): boolean => {
-  console.log(
-    `${character.name} has a crown`,
-    character.inventory.some((item) => item.name === "heavy crown"),
-    character.inventory
-  );
-  return character.inventory.some((item) => item.name === "heavy crown");
-};
+export const hasCrown = (character: Character): boolean =>
+  character.inventory.some((item) => item.name === "heavy crown");
