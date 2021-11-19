b36514c297a6a4888c9e4a4cb97b061c28881b66
Brian Gates <brian.g.gates@gmail.com>
2021-11-18 17:07:11 -0800

    updateMonster

--- src/monster/updateMonster.ts
+++ src/monster/updateMonster.ts
@@ -1,8 +1,10 @@
 import { Monster } from "./Monster";
-import { gameState } from "../gameState";
+import { updateMonster as doUpdateMonster } from "@adventure-bot/store/slices/monsters";
+import store from "@adventure-bot/store";
+import { getMonsterById } from "@adventure-bot/store/selectors";
 
 export const updateMonster = (monster: Monster): Monster => {
   console.log("update monster", monster);
-  gameState.monsters.set(monster.id, monster);
-  return gameState.monsters.get(monster.id) ?? monster;
+  store.dispatch(doUpdateMonster(monster));
+  return getMonsterById(store.getState(), monster.id) ?? monster;
 };
