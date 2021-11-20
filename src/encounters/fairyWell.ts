b9fca45d092301268c64ef98ea55e0afe32e6e1d
Brian Gates <brian.g.gates@gmail.com>
2021-11-20 10:57:35 -0800

    fairy well should report healer quest progress

--- src/encounters/fairyWell.ts
+++ src/encounters/fairyWell.ts
@@ -3,6 +3,7 @@ import { adjustHP } from "../character/adjustHP";
 import { awardXP } from "../character/awardXP";
 import { getUserCharacter } from "../character/getUserCharacter";
 import { hpBarField } from "../character/hpBar/hpBarField";
+import { questProgressField } from "../character/hpBar/hpField";
 import { xpGainField } from "../character/xpGainField";
 import { updateUserQuestProgess } from "../quest/updateQuestProgess";
 
@@ -14,19 +15,22 @@ export const fairyWell = async (
   awardXP(interaction.user.id, 1);
   updateUserQuestProgess(interaction.user, "healer", healAmount);
 
+  const character = getUserCharacter(interaction.user);
   await interaction.reply({
     embeds: [
-      new MessageEmbed()
-        .setTitle("Fairy Well")
-        .setColor("DARK_VIVID_PINK")
-        .setDescription(
-          `You drink from a fairy's well, it heals you for ${healAmount}!`
-        )
-        .addFields([
+      new MessageEmbed({
+        title: "Fairy Well",
+        color: "DARK_VIVID_PINK",
+        description: `You drink from a fairy's well, it heals you for ${healAmount}!`,
+        fields: [
           xpGainField(interaction, 1),
-          hpBarField(getUserCharacter(interaction.user), healAmount),
-        ])
-        .setImage("https://imgur.com/bgq63v9.png"),
+          hpBarField(character, healAmount),
+        ].concat(
+          character.quests.healer
+            ? questProgressField(character.quests.healer)
+            : []
+        ),
+      }).setImage("https://imgur.com/bgq63v9.png"),
     ],
   });
 };
