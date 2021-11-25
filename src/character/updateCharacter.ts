901a23b24013524f6996df21c15dbabbbe8065d5
Brian Gates <brian.g.gates@gmail.com>
2021-11-24 16:27:03 -0800

    fix typo

--- src/character/updateCharacter.ts
+++ src/character/updateCharacter.ts
@@ -1,7 +1,7 @@
 import { Monster } from "../monster/Monster";
 import { Character } from "../character/Character";
 
-import store from '../store'
+import store from "../store";
 import { updateCharacter as doUpdateCharacter } from "../store/slices/characters";
 import { updateMonster as doUpdateMonster } from "../store/slices/monsters";
 import { getCharacterById, getMonsterById } from "../store/selectors";
@@ -14,12 +14,11 @@ export const updateCharacter = (
   if (!character) return;
 
   if (isMonster(character)) {
-    store.dispatch(doUpdateMonster(character))
-    return getCharacterById(store.getState(), character.id)
+    store.dispatch(doUpdateMonster(character));
+    return getMonsterById(store.getState(), character.id);
   } else {
-    
-    store.dispatch(doUpdateCharacter(character))
-    return getMonsterById(store.getState(), character.id)
+    store.dispatch(doUpdateCharacter(character));
+    return getCharacterById(store.getState(), character.id);
   }
-  return character
+  return character;
 };
