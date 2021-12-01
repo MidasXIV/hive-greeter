0ae67ea475b371cff596c534c40e71049f46989a
Brian Gates <brian.g.gates@gmail.com>
2021-12-01 14:28:49 -0800

    windows fix

--- scripts/migrate-db-to-redux-persist.ts
+++ scripts/migrate-db-to-redux-persist.ts
@@ -1,23 +1,36 @@
-import fs from 'fs'
-import path from 'path'
+import fs from "fs";
+import path from "path";
 
 const migrate = async () => {
-  const reduxDBPath = path.join(__dirname, '..', 'db.redux.json')
-  const persistedReduxDBPath = path.join(__dirname, '..', 'db', 'persist:root.json')
+  const reduxDBPath = path.join(__dirname, "..", "db.redux.json");
+  const persistedReduxDBPath = path.join(
+    __dirname,
+    "..",
+    "db",
+    "persist.root.json"
+  );
 
   if (fs.existsSync(reduxDBPath)) {
-    const reduxDBData = JSON.parse(fs.readFileSync(reduxDBPath).toString('utf-8'))
-    
+    const reduxDBData = JSON.parse(
+      fs.readFileSync(reduxDBPath).toString("utf-8")
+    );
+
     const persistedReduxDB = {
-      ...Object.keys(reduxDBData).reduce((acc, k) => ({
-        ...acc,
-        [k]: JSON.stringify(reduxDBData[k]),
-      }), {} as Record<string, string>),
+      ...Object.keys(reduxDBData).reduce(
+        (acc, k) => ({
+          ...acc,
+          [k]: JSON.stringify(reduxDBData[k]),
+        }),
+        {} as Record<string, string>
+      ),
       _persist: `{"version":0,"rehydrated":true}`,
-    }
+    };
 
-    fs.writeFileSync(persistedReduxDBPath, JSON.stringify(persistedReduxDB, null, 2))
+    fs.writeFileSync(
+      persistedReduxDBPath,
+      JSON.stringify(persistedReduxDB, null, 2)
+    );
   }
-}
+};
 
-migrate()
+migrate();
