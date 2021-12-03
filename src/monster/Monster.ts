2f7b62601dcb51986088935fb74372684daf5b12
Brian Gates <brian.g.gates@gmail.com>
2021-12-02 21:26:43 -0800

    cleanup

--- src/monster/Monster.ts
+++ src/monster/Monster.ts
@@ -4,7 +4,5 @@ export type Monster = Character & {
   isMonster: true;
 };
 
-export type NewMonster = Omit<Partial<Monster>, "id"> & { name: string };
-
 export const isMonster = (character: Character): character is Monster =>
   character.isMonster ?? false;
